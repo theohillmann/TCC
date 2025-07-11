0b78680c7c43be5efe26ed6568f97a6ce2f70b05
theo hillmann Luz Coelho <70286183+theohillmann@users.noreply.github.com>
2025-07-10 22:45:33 -0300

    feat: configurate queue

---
+++ queue/config_queue.py
@@ -0,0 +1,17 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+
+channel.exchange_declare(exchange="message_exchange", exchange_type="direct")
+
+channel.queue_declare(queue="text_queue")
+channel.queue_declare(queue="audio_queue")
+
+channel.queue_bind(exchange="message_exchange", queue="text_queue", routing_key="text")
+channel.queue_bind(
+    exchange="message_exchange", queue="audio_queue", routing_key="audio"
+)
+
+print("RabbitMQ configurado com sucesso.")
+connection.close()

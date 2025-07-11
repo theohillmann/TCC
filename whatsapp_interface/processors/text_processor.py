b1abbb40406adeaa84f6af071a85a183190a370c
theo hillmann Luz Coelho <70286183+theohillmann@users.noreply.github.com>
2025-07-10 23:51:19 -0300

    feat: integrate message publishing in TextProcessor

--- whatsapp_interface/processors/text_processor.py
+++ whatsapp_interface/processors/text_processor.py
@@ -1,5 +1,10 @@
+from queue.publisher import publish_message
+
+
 class TextProcessor:
 
     def process(self, message_payload: dict) -> str:
         print(message_payload.get("data").get("message").get("conversation"))
-        return message_payload.get("data").get("message").get("conversation")
+        message = message_payload.get("data").get("message").get("conversation")
+        publish_message("text", message)
+        return message

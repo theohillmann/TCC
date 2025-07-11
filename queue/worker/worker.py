9f3e64274ad9bc8c78c6e42755a17bffb04f5f7e
theo hillmann Luz Coelho <70286183+theohillmann@users.noreply.github.com>
2025-07-10 23:11:37 -0300

    feat: create worker

---
+++ queue/worker/worker.py
@@ -0,0 +1,33 @@
+import pika
+import time
+import argparse
+
+
+def start_worker(queue_name: str):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        print(f"Mensagem recebida da {queue_name}: {body.decode()}")
+        time.sleep(1)
+
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    print(f"[*] Aguardando mensagens em {queue_name}. CTRL+C para sair.")
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Inicia um worker para consumir mensagens de uma fila específica."
+    )
+    parser.add_argument(
+        "queue_name",
+        type=str,
+        help="Nome da fila para consumir (ex: audio_queue, text_queue)",
+    )
+    args = parser.parse_args()
+    queue_name = args.queue_name
+    if queue_name not in ["audio_queue", "text_queue"]:
+        raise ValueError("Insert a valid queue name (audio_queue, text_queue)")
+
+    start_worker(args.queue_name)

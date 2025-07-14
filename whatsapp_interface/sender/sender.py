23700f85b9a98063e17d22730fa517f0e729e87f
theo hillmann Luz Coelho <70286183+theohillmann@users.noreply.github.com>
2025-07-13 22:58:26 -0300

    feat: implement audio_sender method with base64 encoding

--- whatsapp_interface/sender/sender.py
+++ whatsapp_interface/sender/sender.py
@@ -1,7 +1,8 @@
 import os
+import base64
 import dotenv
-import requests
 import random
+import requests
 
 dotenv.load_dotenv()
 API_KEY = os.getenv("AUTHENTICATION_API_KEY")
@@ -13,15 +14,28 @@ class WhatsAppSender:
 
     def __init__(self):
         self.headers = {"apiKey": API_KEY, "Content-Type": "application/json"}
-        self.url = f"http://{self.SERVER_URL}/message/sendText/{self.INSTANCE}"
+        self.text_url = f"http://{self.SERVER_URL}/message/sendText/{self.INSTANCE}"
+        self.audio_url = (
+            f"http://{self.SERVER_URL}/message/sendWhatsAppAudio/{self.INSTANCE}"
+        )
 
     def text_sender(self, message, number):
         payload = {"number": number, "text": message, "delay": self._set_delay()}
-        response = requests.post(url=self.url, json=payload, headers=self.headers)
+        response = requests.post(url=self.text_url, json=payload, headers=self.headers)
         print(response.json())
 
-    def audio_sender(self):
-        pass
+    def audio_sender(self, file_path, number):
+        with open(file_path, "rb") as audio_file:
+            audio_base64 = base64.b64encode(audio_file.read()).decode("utf-8")
+
+        payload = {
+            "number": number,
+            "audio": audio_base64,
+            "delay": self._set_delay(),
+        }
+        response = requests.post(url=self.audio_url, json=payload, headers=self.headers)
+        print(response.json())
+        return response
 
     def _set_delay(self):
         return random.randint(500, 3000)
